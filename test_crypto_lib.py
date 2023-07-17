abf8860f72c3507e3439fd7b8bab4e3ce0c36a7d
Василь Картичак <w.i.k.mailua@gmail.com>
2023-07-17 19:29:18 +0300

    amend test

--- test_crypto_lib.py
+++ test_crypto_lib.py
@@ -4,7 +4,9 @@ import crypto_lib
 
 test_cases_md5 = [
     ('qwerty', 'd8578edf8458ce06fbc5bb76a58c5ca4'),
-    ('66665555', '3f1d68c6ffa30e8ee24493c0f8872b04')
+    ('66665555', '3f1d68c6ffa30e8ee24493c0f8872b04'),
+    ('f', '8fa14cdd754f91cc6554c9e71929cce7'),
+    ('s', '03c7c0ace395d80182db07ae2c30f034'),
 ]
 
 
